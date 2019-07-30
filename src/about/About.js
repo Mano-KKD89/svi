80a3fab2e56355eb42c4bfb37616652be1d88cfd
Muthu <muthu.r@blockgemini.com>
2019-07-30 17:53:02 +0530

    header design

--- src/about/About.js
+++ src/about/About.js
@@ -16,6 +16,7 @@ class About extends Component {
       </>
     );
   }
+  
 }
 
 export default About;
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import logo from "../logo.svg";
+import instagram from "../images/instagram.svg";
+import twitter from "../images/twitter.svg";
+import facebook from "../images/facebook.svg";
+import earth from "../images/earth.svg";
 import {
   Container,
   Row,
@@ -14,6 +18,8 @@ import {
 } from "react-bootstrap";
 import { NavLink, Link } from "react-router-dom";
 import "./header.scss";
+
+
 class Header extends Component {
   constructor() {
     super();
@@ -21,16 +27,16 @@ class Header extends Component {
   render() {
     return (
       <>
-      <Col className="primary">
+      <Col className="header">
        <Row>
-          <Container className="header new">
+          <Container className=" new">
             <Navbar  variant="dark">
               <Navbar.Brand>
                 <Link to="/home">
-                  <img src={logo} style={{ height: "70px" }} />
+                  <img src={logo} style={{ height: "40px" }} />
                 </Link>
               </Navbar.Brand>
-              <Nav className="ml-auto header-link">
+              <Nav className="mx-auto header-link">
                 <NavLink to="/home" activeClassName="active">
                   Home
                 </NavLink>
@@ -49,7 +55,10 @@ class Header extends Component {
                 
               </Nav>
               <ul>
-                <li><a>asas</a></li>
+                <li><NavLink to="#"> <img src={instagram} alt="instagram"/></NavLink></li>
+                <li><NavLink to="#"> <img src={twitter} alt="twitter"/></NavLink></li>
+                <li><NavLink to="#"> <img src={facebook} alt="facebook"/></NavLink></li>
+                <li><NavLink to="#"> <img src={earth} alt="earth"/></NavLink></li>
               </ul>
             </Navbar>
           </Container>
