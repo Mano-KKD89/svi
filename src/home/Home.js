f73be4e76f8030a4300822e7a82fe660281dcb5f
Muthu <muthu.r@blockgemini.com>
2019-08-01 12:30:47 +0530

    home page design

--- src/home/Home.js
+++ src/home/Home.js
@@ -3,14 +3,20 @@ import LandingBg from "../images/Landing-page-Backround.svg";
 import PlayIcon from "../images/play-icon.svg";
 import LandingIllustration from "../images/Landing-page- illustration.svg";
 import Landing2DGraphics from "../images/Landing-2D-Graphics.svg";
+import LandingIllustration2 from "../images/Landing-illustration-2.svg";
 import { Container, Row, Col, Button, Link } from "react-bootstrap";
 import "./home.scss";
 import banner from "../images/about-us.svg";
+
 class Home extends Component {
+
+
   constructor() {
     super();
   }
+
   render() {
+    const { hits } = this.state;
     return (
       <>
         <Row>
@@ -24,11 +30,14 @@ class Home extends Component {
                     <p>Perfect place for your perfect business.</p>
                     <a href=""><img src={PlayIcon} />Watch video</a>
                   </Col>
+
                 </Row>
               </Col>
             </Container>
           </Col>
         </Row>
+
+
         <Row>
           <Col className="middle-section">
             <Container>
@@ -82,6 +91,51 @@ class Home extends Component {
                       </Col>
                     </Row>
                   </Col>
+                  <Col xs={12} className="p-sec">
+                    <p>We offer high quality output with the correct/perfect combination of design,
+                      sound and effects that instantly capture your attention. Our exclusive range of
+                      service includes Graphic designs, 2D and 3D animations, visual services and much more.
+                      The service are delivered as per the clients need at economy cost.</p>
+                  </Col>
+                </Row>
+              </Col>
+            </Container>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="sevice-sec " >
+            <Container>
+              <Col xs={12}>
+                <Row>
+                  <Col xs={6} className="d-flex align-items-center pl-0">
+                    <div className="">
+                      <h1>Get ready for real time services</h1>
+                      <p>Perfect place for your perfect business.</p>
+                      <a href="">Quote Now</a>
+                    </div>
+                  </Col>
+                  <Col xs={6} className="pr-0">
+                    <figure className="img-grap">
+                      <img src={LandingIllustration2} alt="{LandingIllustration2}" />
+                    </figure>
+                  </Col>
+                </Row>
+              </Col>
+            </Container>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="latest-work-sec " >
+            <Container>
+              <Col xs={12}>
+                <Row>
+                  <Col xs={12} className="text-center title-sec">
+                    <h2>Latest<span>Work</span></h2>
+                    <p>We "XYZ" are specialized in understanding your needs and get the desired output/result 
+                      with great elegance. We developed creative designs for different business requirement within the 
+                      stipulated time per
+                    </p>
+                  </Col>
                 </Row>
               </Col>
             </Container>
@@ -90,6 +144,7 @@ class Home extends Component {
       </>
     );
   }
+
 }
 
 export default Home;
