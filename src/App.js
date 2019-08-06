3039da491cfa5a4beda609e97b04e810f359dbc1
manobalan <mano@blockgemini.com>
2019-08-06 14:12:04 +0400

    login token update

--- src/App.js
+++ src/App.js
@@ -9,22 +9,32 @@ import Header from "./header/Header";
 import Admin from "./admin/Admin";
 import { PrivateRoutes } from "./_helpers/PrivateRoutes";
 import Notfound from "./Notfound";
+import jwt_decode from "jwt-decode";
+
+// if (localStorage.getItem("authToken")) {
+//   const tokenData = jwt_decode(localStorage.getItem("authToken"));
+//   let currentDate = Date.now();
+//   if (currentDate > tokenData.exp) {
+//     localStorage.removeItem("authToken");
+//     window.location.href = "/login";
+//   }
+// }
+
 function App() {
   return (
     <>
       <Router>
         <Header />
-         
-            <Col xs={12} className="Whole-container">
-              <Switch>
-                {routes.map(url => {
-                  return <Route exact {...url} />;
-                })}
-                <PrivateRoutes exact path="/admin" component={Admin} />
-                <Route component={Notfound} />
-              </Switch>
-            </Col>
-         
+
+        <Col xs={12} className="Whole-container">
+          <Switch>
+            {routes.map(url => {
+              return <Route exact {...url} />;
+            })}
+            <PrivateRoutes exact path="/admin" component={Admin} />
+            <Route component={Notfound} />
+          </Switch>
+        </Col>
       </Router>
     </>
   );
