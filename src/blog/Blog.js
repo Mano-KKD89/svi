544a1617f34cbef4eca5be596a39748c19853b0b
manobalan <balamano1989@gmail.com>
2019-08-13 14:08:05 +0400

    blog page getting data from backend

--- src/blog/Blog.js
+++ src/blog/Blog.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import fire from '../firebase';
 import "./blog.scss";
 class Blog extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      blogData: []
+    }
+  }
+  componentDidMount = () => {
+    const databaseRef = fire.database().ref('blog');
+    databaseRef.on('value', (snapshot) => {
+      if(snapshot.val()) {
+        const details = Object.values(snapshot.val());
+        let newData = [];
+        for(let d of details) {
+          newData.push({
+            id: d.blog_id,
+            title: d.blog_title,
+            description: d.blog_description,
+            link: d.blog_link,
+            category: d.blog_category,
+            imgpath: d.blog_img,
+          })
+        }
+        this.setState({
+          blogData: newData
+        })
+      }
+    })
   }
   render() {
     return (
@@ -12,6 +38,33 @@ class Blog extends Component {
           <Col xs={12}>
             <h3>Blog</h3>
           </Col>
+          <Col xs={12} className="inner-sec">
+                    <Row>
+                      {this.state.blogData.length ? (
+                        <>
+                        {this.state.blogData.map(data => {
+                        return (
+                          <Col xs={3}>
+                          <div key={data.id} className="works-div mt-3 mb-3">
+                            <figure className="img-grap">
+                              <img src={data.imgpath} alt={data.category} title={data.category} />
+                            </figure>
+                            <div className="works-info">
+                              <h5>{data.category}</h5>
+                              <h3>{data.title}</h3>
+                              <p>{data.description}</p>
+                            </div>
+                          </div>
+                        </Col>
+                        )
+                        })}
+                       
+                      </>
+                      ) : null }
+                      
+
+                      </Row>
+                      </Col>
         </Row>
       </>
     );
